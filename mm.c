941b302a52c80094367af478fb7eca010398a11c
Ben <bks5525@psu.edu>
2021-09-30 20:33:05 -0400

    implemented free

--- mm.c
+++ mm.c
@@ -68,6 +68,7 @@ struct header {
     struct header *next_block;
     struct header *prev_block;
 };
+typedef struct header block_header;
 
 /*
  * Initialize: returns false on error, true on success.
@@ -75,7 +76,7 @@ struct header {
 bool mm_init(void)
 {
     /* IMPLEMENT THIS */
-    struct header *ptr = mem_sbrk(16);
+    struct header *ptr = mem_sbrk(align(sizeof(block_header)));
     //size_t size = 16;
     ptr->next_block = ptr;
     ptr->prev_block = ptr;
@@ -128,12 +129,19 @@ void* find_open_block(size_t size) {
 void free(void* ptr)
 {
     /* IMPLEMENT THIS */
-    // this should do nothing if ptr has a value of NULL
-    if (ptr == NULL) {
-        return;
+    if (ptr != NULL) {
+        /* we want the contents of the ptr without the header content and we also need a pointer to the 
+        head of the free list */
+        block_header *blockPtr = ptr - align(sizeof(block_header));
+        block_header *head = mem_heap_lo();
+        // *foot = mem_heap_hi(); eventually we will need to implement this  
+        // free up the block and change block pointers to point to the next free block 
+        blockPtr->size &= ~1;
+        blockPtr->next_block = head->next_block;
+        blockPtr->prev_block = head;
+        head->next_block = blockPtr;
+        blockPtr->next_block->prev_block = blockPtr;
     }
-
-
     return;
 }
 
